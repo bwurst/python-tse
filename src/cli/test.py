3d3c16ee07f957a11a930d520fea6ddc45c2d65e
Bernd Wurst <bernd@bwurst.org>
2020-01-16 13:24:26 +0100

    Finde den Pfad zur Library selbst

--- src/cli/test.py
+++ src/cli/test.py
@@ -1,6 +1,16 @@
+import sys
+import os.path
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'lib'))
+
 from worm import Worm
 
-worm = Worm('/mnt/tse')
+if len(sys.argv) < 2:
+    print('usage: %s <mountpoint>' % (sys.argv[0],))
+    sys.exit()
+
+mountpoint = sys.argv[1]
+worm = Worm(mountpoint)
+
 print(worm.getVersion())
 
 
