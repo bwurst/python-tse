21c238abb0850d93dade1780d4a63e6a39ae9b41
Bernd Wurst <bernd@bwurst.org>
2020-01-16 10:29:58 +0100

    definiere argtypes und codiere mountpoint korrekt

--- src/lib/worm.py
+++ src/lib/worm.py
@@ -7,16 +7,26 @@ class Worm:
     
     def __init__(self, mountpoint):
         self.wormlib = cdll.LoadLibrary(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../so/libWormAPI.so'))
-        # FIXME: WormContext
         self.ctx = WormContext()
-        # FIXME: worm_init()
-        ret = self.wormlib.worm_init(self.ctx, mountpoint)
+        self.wormlib.worm_init.argtypes = (WormContext, c_char_p)
+        self.wormlib.worm_init.restype = WormError
+        ret = self.wormlib.worm_init(self.ctx, mountpoint.encode('utf-8'))
+        # FIXME: returncode auswerten
         print('return code for worm_init() => ', ret)
+
+
+    def __del__(self):
+        self.wormlib.worm_cleanup.argtypes = (WormContext,)
+        self.wormlib.worm_cleanup.restype = WormError
+        ret = self.wormlib.worm_cleanup(self.ctx)
+        # FIXME: returncode auswerten
+        print('return code for worm_cleanup() => ', ret)
+
 
     def getVersion(self):
         self.wormlib.worm_getVersion.restype = c_char_p
         ret = self.wormlib.worm_getVersion()
-        # liefert bytes
+        # liefert bytes. Wir konvertieren als latin1, da es da keine UnicodeDecodeErrors geben kann.
         return ret.decode('latin1')
         
     
--- src/lib/wormtypes.py
+++ src/lib/wormtypes.py
@@ -4,3 +4,8 @@ from ctypes import POINTER, c_char
 # FIXME: Das sollte ein struct sein, wir wissen aber nicht was alles darin sein muss.
 
 WormContext = POINTER(c_char)
+
+
+
+WormError = int
+
