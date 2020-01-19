8d0a1604cf494b48cabf5b2e3ce78b4936b82d3b
Leon <leon@fairtragen.de>
2020-01-19 10:23:10 +0100

    gitignore etc.

--- src/cli/test.py
+++ src/cli/test.py
@@ -12,10 +12,10 @@ worm = Worm(config.path_tse)
 #print(worm.getVersion())
 #print(worm.signatureAlgorithm())
 
-#print(worm.info_read())
+print(worm.info_read())
 #print(worm.info_capacity()) # läuft nicht (wahrscheinlich weil WormInfo nicht korrekt definiert ist)
 
-print(worm.flash_health_summary()) # läuft nicht
+#print(worm.flash_health_summary()) # läuft nicht richtig
 
 
 
