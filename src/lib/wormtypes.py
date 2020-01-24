2114f596409c1630e914e3c364143786b4034c7c
Leon <leon@fairtragen.de>
2020-01-24 07:31:50 +0100

    Reihenfolge der Felder in WormInfo geändert

--- src/lib/wormtypes.py
+++ src/lib/wormtypes.py
@@ -6,7 +6,7 @@ from ctypes import *
 #class WormContext(pointer(c_int)):
 #    pass
 
-#WormContext = pointer(c_int)
+#WormContext = pointer(c_int())
 
 # class WormContext():
 #     ctx = pointer(c_int())
@@ -32,18 +32,20 @@ class WormInfo(Structure):
     wormInitializationState = WormInitializationState() # FIXME: das funktionert so noch nicht
 
     _fields_ = [
+                ("customizationIdentifier", c_ubyte), # original: std::vector< unsigned char >
                 ("isDevelopmentFirmware", c_bool),
+                ("capacity", c_uint32),
+                ("size", c_int32),
                 ("hasValidTime", c_bool),
                 ("hasPassedSelfTest", c_bool),
                 ("isCtssInterfaceActive", c_bool),
                 ("isExportEnabledIfCspTestFails", c_bool),
+                ("initializationState", c_int),  # wormInitializationState),
                 ("isDataImportInProgress", c_bool),
                 ("hasChangedPuk", c_bool),
                 ("hasChangedAdminPin", c_bool),
                 ("hasChangedTimeAdminPin", c_bool),
-
-                ("maxRegisteredClients", c_uint32),
-                ("registeredClients", c_uint32),
+                ("timeUntilNextSelfTest", c_uint32),
                 ("startedTransactions", c_uint32),
                 ("maxStartedTransactions", c_uint32),
                 ("createdSignatures", c_uint32),
@@ -51,23 +53,17 @@ class WormInfo(Structure):
                 ("remainingSignatures", c_uint32),
                 ("maxTimeSynchronizationDelay", c_uint32),
                 ("maxUpdateDelay", c_uint32),
+                ("tsePublicKey", c_ubyte), # original: std::vector< unsigned char >
+                ("tseSerialNumber", c_ubyte), # original: std::vector< unsigned char >
+                ("tseDescription", c_wchar_p),  # original: std::string
+                ("registeredClients", c_uint32),
+                ("maxRegisteredClients", c_uint32),
                 ("certificateExpirationDate", c_uint64),
                 ("tarExportSizeInSectors", c_uint64),
                 ("tarExportSize", c_uint64),
                 ("hardwareVersion", c_uint32),
                 ("softwareVersion", c_uint32),
-                ("size", c_int32),
-                ("capacity", c_uint32),
-                ("timeUntilNextSelfTest", c_uint32),
-                
-                ("tsePublicKey", c_ubyte), # original: std::vector< unsigned char >
-                ("tseSerialNumber", c_ubyte), # original: std::vector< unsigned char >
-                ("customizationIdentifier", c_ubyte), # original: std::vector< unsigned char >
-                
-                ("tseDescription", c_wchar_p),  # original: std::string
                 ("formFactor", c_wchar_p),  # original: std::string
-                
-                ("initializationState", c_int),  # wormInitializationState),
                 ]
 
 
